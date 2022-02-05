7315a03a6eff0a9bf4ed5d8208f9bd7284b2db09
zenidey <93101838+zenidey@users.noreply.github.com>
2022-02-04 22:46:26 -0800

    class product added

--- index.js
+++ index.js
@@ -1,102 +1,102 @@
-class House {
+class Box {
     constructor(name) {
         this.name = name;
         this.room = [];
     }
 
-    addRoom(name, area) {
-        this.room.push(new Room(name, area));
+    addProduct(name, area) {
+        this.room.push(new Product(name, area));
     }
 }
 
-class Room {
+class Product {
     constructor(name, area) {
         this.name = name;
         this.area = area;
     }
 }
 
-class HouseService { 
-    // static url = 'https://crudcrud.com/api/e1d6e8870ff74aa9a79bf5a8de2f8d06/unicorns';
+
+class BoxService {        
     static url = 'https://ancient-taiga-31359.herokuapp.com/api/houses'; 
 
-    static getAllHouses() {     
+    static getAllBoxes() {         
         return $.get(this.url);
     }
 
-    static getHouse(id) {          
+    static getBox(id) {         
         return $.get(this.url + `/${id}`);
     }
 
-    static createHouse(house) {  
-        return $.post(this.url, house);
+    static createBox(box) {    
+        return $.post(this.url, box);
     }
 
-    static updateHouse(house) {     
-        return $.ajax({    
-            url: this.url + `/${house._id}`,  
+    static updateBox(box) {     
+        return $.ajax({   
+            url: this.url + `/${box._id}`,   
             dataType: 'json',   
-            data: JSON.stringify(house), 
+            data: JSON.stringify(box), 
             contentType: 'application/json',
             type: 'PUT' 
         });
     }
 
-    static deleteHouse(id) {
+    static deleteBox(id) {
         return $.ajax({
             url: this.url + `/${id}`, 
-            type: 'DELETE' 
+            type: 'DELETE'  
         });
     }
 }
 
 class DOMManager {
-    static houses;
+    static boxes;
 
-    static getAllHouses() {
-        HouseService.getAllHouses().then(houses => this.render(houses));
+    static getAllBoxes() {
+        BoxService.getAllBoxes().then(boxes => this.render(boxes));
     }
 
-    static createHouse(name) {
-        HouseService.createHouse(new House(name))
+    static createBox(name) {
+        BoxService.createBox(new Box(name))
         .then(() => {
-            return HouseService.getAllHouses();
+            return BoxService.getAllBoxes();
         })
-        .then((houses) => this.render(houses));
+        .then((boxes) => this.render(boxes));
     }
 
-    static deleteHouse(id) {
-        HouseService.deleteHouse(id) 
+    static deleteBox(id) {
+        BoxService.deleteBox(id) 
         .then(() => {
-            return HouseService.getAllHouses();
+            return BoxService.getAllBoxes();
         })
-        .then((houses) => this.render(houses));
+        .then((boxes) => this.render(boxes));
     }
 
-    static addRoom(id) {
-        for (let house of this.houses) {
-            if (house._id == id) {
-                house.rooms.push(new Room($(`#${house._id}-room-name`).val(), $(`#${house._id}-room-area`).val()));
-                HouseService.updateHouse(house)
+    static addProduct(id) {
+        for (let box of this.boxes) {
+            if (box._id == id) {
+                box.rooms.push(new Product($(`#${box._id}-room-name`).val(), $(`#${box._id}-room-area`).val()));
+                BoxService.updateBox(box)
                     .then(() => {
-                        return HouseService.getAllHouses();
+                        return BoxService.getAllBoxes();
                     })
-                    .then((houses) => this.render(houses));
+                    .then((boxes) => this.render(boxes));
             }
         }
     }
 
-    static deleteRoom(houseId, roomId) {
-        for (let house of this.houses) {
-            if(house._id == houseId) {
-                for (let room of house.rooms) {
+    static deleteProduct(boxId, roomId) {
+        for (let box of this.boxes) {
+            if(box._id == boxId) {
+                for (let room of box.rooms) {
                     if (room._id == roomId){
-                        house.rooms.splice(house.rooms.indexOf(room), 1);
-                        HouseService.updateHouse(house)
+                        box.rooms.splice(box.rooms.indexOf(room), 1);
+                        BoxService.updateBox(box)
                         .then(() => {
-                            return HouseService.getAllHouses();
+                            return BoxService.getAllBoxes();
                         })
-                        .then((houses) => this.render(houses));
+                        .then((boxes) => this.render(boxes));
                     }
                 }
             }
@@ -104,47 +104,47 @@ class DOMManager {
     }
 
 
-    static render(houses) {
-        this.houses = houses;
+    static render(boxes) {
+        this.boxes = boxes;
         $('#app').empty();
-        for (let house of houses) {
+        for (let box of boxes) {
             $('#app').prepend(
-                `<div id="${house._id}" class="card">
+                `<div id="${box._id}" class="card">
                     <div class="card-header">
-                        <h2>${house.name}</h2>
-                        <button class="btn btn-danger" onclick="DOMManager.deleteHouse('${house._id}')">Delete</button>
+                        <h2>${box.name}</h2>
+                        <button class="btn btn-danger" onclick="DOMManager.deleteBox('${box._id}')">Delete</button>
                     </div>
                     <div class="card-body">
                         <div class="card">
                             <div class="row">
                                 <div class="col-sm">
-                                    <input type="text" id="${house._id}-room-name" class ="form-control" placeHolder="Room Name">
+                                    <input type="text" id="${box._id}-room-name" class ="form-control" placeHolder="Product Name">
                                 </div>
                                 <div class="col-sm">
-                                <input type="text" id="${house._id}-room-area" class ="form-control" placeHolder="Room Area">
+                                <input type="text" id="${box._id}-room-area" class ="form-control" placeHolder="Product Area">
                                 </div>
                             </div>
-                            <button id="${house._id}-new-room" onclick="DOMManager.addRoom('${house._id}')" class="btn btn-primary form-control">Add</button>
+                            <button id="${box._id}-new-room" onclick="DOMManager.addProduct('${box._id}')" class="btn btn-primary form-control">Add</button>
                         </div>
                     </div>
                 </div> <br>`
             );
 
-            for (let room of house.rooms) {
-                $(`#${house._id}`).find('.card-body').append(
+            for (let room of box.rooms) {
+                $(`#${box._id}`).find('.card-body').append(
                     `<p>
-                    <span id="name-${room._id}"><strong>Name: </strong> ${room.name}</span>
-                    <span id="area-${room._id}"><strong>Area: </strong> ${room.area}</span>
-                    <button class="btn btn-danger" onclick="DOMManager.deleteRoom('${house._id}', '${room._id}')">Delete Room</button></p>`
+                    <span id="name-${room._id}"><strong>product: </strong> ${room.name}</span>
+                    <span id="area-${room._id}"><strong>price: </strong> ${room.area}</span>
+                    <button class="btn btn-danger" onclick="DOMManager.deleteProduct('${box._id}', '${room._id}')">Delete Product</button></p>`
                 );
             }
         }
     }
 }
 
-$('#create-new-house').click(() => {
-    DOMManager.createHouse($('#new-house-name').val());
-    $('#new-house-name').val('');
+$('#create-new-box').click(() => {
+    DOMManager.createBox($('#new-box-name').val());
+    $('#new-box-name').val('');
 })
 
-DOMManager.getAllHouses();
+DOMManager.getAllBoxes();
